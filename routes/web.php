a7d9be3db426ea08da557347cf96e4e0f4e499eb
DESKTOP-MQLDKIM\Lenovo <jidanmutaqin73@smk.belajar.id>
2022-02-02 15:45:52 +0800

    Menambah Create Member

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaketController;
+use App\Http\Controllers\MemberController;
 use App\Http\Controllers\OutletController;
 
 /*
@@ -21,3 +22,4 @@ Route::get('/', function () {
 
 Route::resource('/outlet', OutletController::class);
 Route::resource('/paket', PaketController::class);
+Route::resource('/membert', MemberController::class);
